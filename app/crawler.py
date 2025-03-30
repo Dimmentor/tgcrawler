4fa351e20ea0cbacad9a88bb8966739937d4590b
Dmitry Longinov <dl.ruled@gmail.com>
2025-03-31 00:30:13 +0900

    requirements.txt fix

--- app/crawler.py
+++ app/crawler.py
@@ -14,10 +14,12 @@ async def crawling_prices(url: str, xpath: str) -> float:
     options = webdriver.ChromeOptions()
     options.add_argument(
         "user-agent=Mozilla/5.0 (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--ignore-certificate-errors")
+    options.add_argument('--ignore-ssl-errors')
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("pageLoadStrategy=none")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     price = None
